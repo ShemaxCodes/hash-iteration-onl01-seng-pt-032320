950b405c1380a4a9206e829125bda5c363cf3113
Jashema Panapa <jcpanapa@gmail.com>
2020-04-02 03:54:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -24,5 +24,8 @@ birthday_kids = {
   "Sarah" => 6,
   "Amanda" => 27
 }
-
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
